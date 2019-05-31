6a76f0ad56c08b66d922e2a25a7a9644e5aca11a
Amado Jose D Roque <amado.roque@pirelli.com>
2019-05-30 21:38:05 -0300

    some commit

---
+++ ctrl.js
@@ -0,0 +1,4 @@
+angular.module('BlankApp', ['ngMaterial', 'ngMessages'])
+    .controller('ctrlM', function($scope){
+        $scope.appTitle = 'TECNOBIN: Como reduzir o E-lixo no Brasil e no mundo'
+    });
